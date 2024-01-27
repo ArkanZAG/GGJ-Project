e79aeda29b22124d68efa361432609a04476c0bd
jasonjs98 <jason.marvel200@gmail.com>
2024-01-27 12:10:58 +0700

    scene load
    update button untuk main menu dan game over

--- Assets/Scripts/Main Menu/Play.cs
+++ Assets/Scripts/Main Menu/Play.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+
+
 
 public class SceneLoad : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    private string gameScene = "GameScene";
+    private string menuScene = "MainMenuScene";
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Button menuButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(LoadScene);
+        menuButton.onClick.AddListener(MainMenu);
+    }
 
     void LoadScene()
     {
-        SceneManager.LoadScene("Scenes/GameScene");
+        SceneManager.LoadScene(gameScene);
+        Debug.Log("berhasil");
+    }
+
+    void QuitScene()
+    {
+        Application.Quit();
+    }
+    
+    void MainMenu()
+    {
+        SceneManager.LoadScene(menuScene);
+        Debug.Log("berhasil");
     }
 }
