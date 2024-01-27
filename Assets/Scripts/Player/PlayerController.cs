436c5b615e61ee441fbf80a87f9e18c45c17c70d
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2024-01-27 12:11:56 +0700

    Animation #1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -25,7 +25,16 @@ public class PlayerController : MonoBehaviour
 
         playerRigidBody.velocity = new Vector2(horizontalInput * movementSpeed, playerRigidBody.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpHeight);
         }
