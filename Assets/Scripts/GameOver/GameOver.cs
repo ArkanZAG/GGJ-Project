ec324d5384ce07f00c42166547f9c990469a78b3
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2024-01-28 07:48:42 +0700

    fix gameovertext bug

--- Assets/Scripts/GameOver/GameOver.cs
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -8,45 +8,48 @@ using System;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using System.Runtime.CompilerServices;
+using Random = System.Random;
 
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI gameOverQuote;
+    private string[] gameOverString = 
+            {
+                "Gapapa pertama kali coba",
+                "Jangan ngulang kesalahan yang sama",
+                "Yah kasian udah jauh malah mati",
+                "Hapalin jebakan-jebakannya",
+                "Never give up",
+                "Jangan mudah menyerah walaupun sulit",
+                "Focus",
+                "Hati-hati",
+                "Awas jatuh",
+                "Minum air biar fokus",
+                "Kamu belajar dari kesalahan ga sih?...",
+                "Skill issue",
+                "Landaknya Lucu",
+                "Koin bukan segalanya",
+                "Hampir",
+                "Coba ganti usernya",
+                "Udah nolep kamu",
+                "Santai aja ga usah buru-buru",
+                "Semua masalah pasti ada jalannya",
+                "Never gonna give you up",
+                "Yah ngulang lagi",
+                "Jangan buang-buang waktu kamu:D"
+            };
     public void GetRandomText()
     {
-        string[] gameOverString = 
+        if (gameOverString.Length > 0)
         {
-            "Gapapa pertama kali coba",
-            "Jangan ngulang kesalahan yang sama",
-            "Yah kasian udah jauh malah mati",
-            "Hapalin jebakan-jebakannya",
-            "Never give up",
-            "Jangan mudah menyerah walaupun sulit",
-            "Focus",
-            "Hati-hati",
-            "Awas jatuh",
-            "Minum air biar fokus",
-            "Kamu belajar dari kesalahan ga sih?...",
-            "Skill issue",
-            "Landaknya Lucu",
-            "Koin bukan segalanya",
-            "Hampir",
-            "Coba ganti usernya",
-            "Udah nolep kamu",
-            "Santai aja ga usah buru-buru",
-            "Semua masalah pasti ada jalannya",
-            "Never gonna give you up",
-            "Yah ngulang lagi",
-            "Jangan buang-buang waktu kamu:D"
-        };
-
-        System.Random random = new System.Random();
-    
-        for (int i = 0; i < gameOverString.Length; i++)
+            int randomIndex = UnityEngine.Random.Range(0, gameOverString.Length);
+            string randomQuote = gameOverString[randomIndex];
+            gameOverQuote.text = randomQuote;
+        }
+        else
         {
-            int randomIndex = random.Next(0, gameOverString.Length);
-            Debug.Log(gameOverString[randomIndex]);
-         }
+            Debug.LogWarning("Array Kosong");
+        }
     }
 
     private void Start()
